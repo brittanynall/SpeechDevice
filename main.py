8a72e617df3c388a410d125651c4885a32b6e8d4
Suraj Deuja <deujasuraj77@gmail.com>
2016-03-13 19:58:51 -0600

    added rpc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtGui, QtCore, QtWidgets, uic
 from PyQt5.QtCore import pyqtSlot
+from NetworkService import NetworkService
+from RpcService import RpcServer
+from threading import Thread
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
@@ -11,13 +14,20 @@ class Main(QtWidgets.QMainWindow):
     def button_clicked(self):
         self.ui.pushButton.setText("Hello!")
 
-
 if __name__ == '__main__':
     import sys
  
     app = QtWidgets.QApplication(sys.argv)
 
-    screen = Main()
-    screen.show()
- 
-    sys.exit(app.exec_())
+    # instantiate new window
+    main_window = Main()
+    main_window.show()
+
+    # start the rpc server
+    try:
+        server = RpcServer(main_window)
+        server_thread = Thread(target=server.start)
+        server_thread.start()
+        sys.exit(app.exec_())
+    except KeyboardInterrupt:
+        pass
